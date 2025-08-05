6e97d0e1810c7d814dc9cbe9fc3f1476de5ff11a
Ken C. Demanawa <kenneth.c.demanawa@gmail.com>
2025-08-05 14:38:50 +0800

    initial files

---
+++ lib/prescient.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "prescient/version"
+
+module Prescient
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/prescient/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Prescient
+  VERSION = "0.0.0"
+end
